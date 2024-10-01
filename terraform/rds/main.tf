b2ba19299f9a0277a6bdba1b79175d4050e1eb18
Thomas Tusia <95367414+ttusia-cisco@users.noreply.github.com>
2024-10-01 10:53:05 -0600

    password

--- terraform/rds/main.tf
+++ terraform/rds/main.tf
@@ -4,7 +4,7 @@ resource "aws_db_instance" "my_db" {
   engine_version    = "5.7"
   instance_class    = "db.t3.micro"
   username          = "root"
-  password          = "my_super_secret_password"
+  #password          = "my_super_secret_password"
   manage_master_user_password = true
   #storage_encrypted = false
   #tags = {
